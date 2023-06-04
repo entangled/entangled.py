11fc26ccd85de325a715b3bc31d3fafbe6a8fa22
Johan Hidding <j.hidding@esciencecenter.nl>
2023-06-04 22:15:03 +0200

    make cycles testing in-memory

--- test/test_cycles.py
+++ test/test_cycles.py
@@ -2,13 +2,51 @@ from pathlib import Path
 import pytest
 
 from entangled.tangle import tangle_ref
-from entangled.markdown_reader import read_markdown
+from entangled.markdown_reader import MarkdownReader
 from entangled.errors.user import CyclicReference
 from entangled.document import AnnotationMethod
 
 
+md_source = """
+This should raise a `CyclicReference` error.
+
+``` {.python #hello}
+<<hello>>
+```
+
+So should this:
+
+``` {.python #phobos}
+<<deimos>>
+```
+
+``` {.python #deimos}
+<<phobos>>
+```
+
+also when tangling from something else:
+
+``` {.python #mars}
+<<phobos>>
+```
+
+What should not throw an error is doubling a reference:
+
+``` {.python #helium}
+<<electron>>
+<<electron>>
+```
+
+``` {.python #electron}
+negative charge
+```
+"""
+
+
 def test_cycles():
-    refs, _ = read_markdown(Path(__file__).parent / "data" / "cyclic.md")
+    mr = MarkdownReader("-")
+    mr.run(md_source)
+    refs = mr.reference_map
 
     with pytest.raises(CyclicReference):
         tangle_ref(refs, "hello")
