ec233a33692fe48175641c61f2501e9eefd6914b
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2023-08-01 11:20:03 +0200

    status: use rich styles + factor out panel creation

--- entangled/commands/status.py
+++ entangled/commands/status.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from ..status import find_watch_dirs, list_input_files, list_dependent_files
 from ..config import config
 from pathlib import Path
@@ -7,7 +8,6 @@ try:
     from rich.columns import Columns
     from rich.table import Table
     from rich.panel import Panel
-    from rich.rule import Rule
     from rich.tree import Tree
 
     WITH_RICH = True
@@ -19,11 +19,16 @@ def tree_from_files(files):
     tree = Tree(label=".")
     dirs = { Path("."): tree }
     for f in sorted(files):
-        for p in f.parents[::-1]:
+        for p in reversed(f.parents):
             if p not in dirs:
-                dirs[p] = dirs[p.parent].add(p.name)
-        dirs[f.parent].add(f.name)
+                dirs[p] = dirs[p.parent].add(p.name, style="repr.path")
+        dirs[f.parent].add(f.name, style="repr.filename")
     return tree
+
+
+def files_panel(file_list: list[str], title: str) -> Panel:
+    tree = tree_from_files(file_list)
+    return Panel(tree, title=title, border_style="dark_cyan")
 
 
 def rich_status():
@@ -37,9 +42,11 @@ def rich_status():
     console = Console(color_system="auto")
     group = Group(
         Panel(config_table, title="config", border_style="dark_cyan"),
-        Columns((
-            Panel(tree_from_files(list_input_files()), title="input files", border_style="dark_cyan"),
-            Panel(tree_from_files(list_dependent_files()), title="dependent files", border_style="dark_cyan"))))
+        Columns([
+            files_panel(list_input_files(), "input files"),
+            files_panel(list_dependent_files(), "dependent files"),
+        ]),
+    )
 
     console.print(group)
 
