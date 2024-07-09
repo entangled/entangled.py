a0ce3019e2fa8f5ce6e3433cd7f87a8484078aa8
Johan Hidding <j.hidding@esciencecenter.nl>
2024-07-09 09:04:14 +0200

    fix crashes on Path.glob

--- entangled/status.py
+++ entangled/status.py
@@ -5,6 +5,17 @@ from itertools import chain
 from pathlib import Path
 
 
+def safe_glob(pattern):
+    """The `glob` method of `Path` raises a FileNotFound error when
+    there are rapid changes in the filesystem.
+
+    `safe_glob` catches this error and returns an empty list instead."""
+    try:
+        return Path().glob(pattern)
+    except FileNotFoundError:
+        return []
+
+
 def find_watch_dirs():
     """List all directories that contain files that need watching."""
     input_file_list = list_input_files()
@@ -16,9 +27,9 @@ def find_watch_dirs():
 
 def list_input_files():
     """List all input files."""
-    include_file_list = chain.from_iterable(map(Path().glob, config.watch_list))
+    include_file_list = chain.from_iterable(map(safe_glob, config.watch_list))
     exclude_file_list = list(
-        chain.from_iterable(map(Path().glob, config.ignore_list))
+        chain.from_iterable(map(safe_glob, config.ignore_list))
     )
     return [path for path in include_file_list if not path in exclude_file_list]
 
