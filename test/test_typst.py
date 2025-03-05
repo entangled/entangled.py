2f4941d0e5dfa9537e8071e9fa0b6bc34e9d56c9
Johan Hidding <j.hidding@esciencecenter.nl>
2025-03-05 07:45:12 +0100

    actually import sys in typst test

--- test/test_typst.py
+++ test/test_typst.py
@@ -2,6 +2,8 @@ from contextlib import chdir
 from subprocess import run
 from pathlib import Path
 from shutil import copytree
+import sys
+
 
 def test_typst(tmp_path: Path, data: Path):
     copytree(data / "typst", tmp_path / "typst")
