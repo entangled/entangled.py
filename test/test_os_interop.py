49b1813c9d3e3b3300fd62e27f81634360d97c21
Johan Hidding <j.hidding@esciencecenter.nl>
2025-03-05 11:39:44 +0100

    test os interop with similar to hello-world test, but with paths

---
+++ test/test_os_interop.py
@@ -0,0 +1,23 @@
+from entangled.markdown_reader import read_markdown_file
+from entangled.tangle import tangle_ref
+from entangled.code_reader import CodeReader
+
+from pathlib import Path
+import os
+from shutil import copytree, move
+from contextlib import chdir
+
+
+def test_tangle_ref(data, tmp_path):
+    copytree(data / "os_interop", tmp_path / "os_interop")
+    with chdir(tmp_path / "os_interop"):
+        refs, _ = read_markdown_file(Path("doc/index.md"))
+        tangled, deps = tangle_ref(refs, "src/euler_number.c")
+        assert deps == {"doc/index.md"}
+        with open("src/euler_number.c", "r") as f:
+            assert f.read().strip() == tangled.strip()
+
+        cb_old = next(refs["series-expansion"]).source
+        cr = CodeReader("-", refs).run(Path("src/euler_number.c.edit").read_text())
+        cb_new = next(refs["series-expansion"]).source
+        assert cb_old != cb_new
