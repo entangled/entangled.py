28dd2f10b02e9949c2e263172243ec25f76aa7a4
Johan Hidding <j.hidding@esciencecenter.nl>
2025-03-16 07:48:44 +0100

    fix shebang hook crash on empty code blocks

--- entangled/hooks/shebang.py
+++ entangled/hooks/shebang.py
@@ -5,6 +5,6 @@ from .base import HookBase
 class Hook(HookBase):
     def on_read(self, code: CodeBlock):
         lines = code.source.splitlines()
-        if lines[0].startswith("#!"):
+        if lines and lines[0].startswith("#!"):
             code.header = lines[0]
             code.source = "\n".join(lines[1:])
