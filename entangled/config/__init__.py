4a436794c9fdd8f2750fb5bd8048a8a6fedd3b29
Johan Hidding <j.hidding@esciencecenter.nl>
2025-04-25 00:57:15 +0200

    change default ignore_list to empty list

--- entangled/config/__init__.py
+++ entangled/config/__init__.py
@@ -83,7 +83,7 @@ class Config(threading.local):
     languages: list[Language] = field(default_factory=list)
     markers: Markers = field(default_factory=lambda: copy(markers))
     watch_list: list[str] = field(default_factory=lambda: ["**/*.md"])
-    ignore_list: list[str] = field(default_factory=lambda: ["README.md"])
+    ignore_list: list[str] = field(default_factory=list)
     annotation_format: Optional[str] = None
     annotation: AnnotationMethod = AnnotationMethod.STANDARD
     use_line_directives: bool = False
