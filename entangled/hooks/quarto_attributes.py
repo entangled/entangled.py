a08058c04785772e81eeca5d13a3b38e2e92f508
Johan Hidding <j.hidding@esciencecenter.nl>
2025-10-11 21:35:36 +0200

    quatro attributes are now stripped when tangling; only top consecutive lines are used; more lenient towards extra spaces; fix #71

--- entangled/hooks/quarto_attributes.py
+++ entangled/hooks/quarto_attributes.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+import re
+import textwrap
 from typing import Any, final, override
 import yaml
 
@@ -13,33 +15,63 @@ from ..logging import logger
 log = logger()
 
 
-def get_code_properties(source: str, language: Language) -> list[Property]:
+def split_yaml_header(language: Language, source: str) -> tuple[str, str, object]:
+    """Split source into YAML header and body."""
+    trigger = re.escape(language.comment.open) + r"\s*\|(.*)"
+    lines = source.splitlines()
+    header_lines: list[str] = []
+    body_start: int = 0
+
+    for i, line in enumerate(lines):
+        if m := re.match(trigger, line):
+            header_lines.append(m.group(1))
+            continue
+
+        body_start = i
+        break
+
+    return "\n".join(lines[:body_start]), "\n".join(lines[body_start:]), yaml.safe_load("\n".join(header_lines))
+
+
+def amend_code_properties(code_block: CodeBlock):
+    language = code_block.language
+    if not language:
+        return
+
+    source = code_block.source
     props: list[Property] = []
 
-    trigger = f"{language.comment.open}|"
-    header = "\n".join(
-        line[len(trigger):] for line in source.splitlines()
-        if line.startswith(trigger))
+    header, body, attrs = split_yaml_header(language, source)
 
-    attrs = yaml.safe_load(header)
     if attrs is None:
-        return props
+        return
+
     if not isinstance(attrs, dict):
-        log.warning("quarto attributes do not evaluate to dictionary; skipping")
-        log.warning(f"tried to parse: {header}")
-        return props
+        log.warning(f"{code_block.origin}: Quarto attributes do not evaluate to dictionary; skipping")
+        log.warning(f"tried to parse:\n{header}")
+        return
 
     if "id" in attrs.keys():
-        assert isinstance(attrs["id"], str)
+        if not isinstance(attrs["id"], str):
+            log.warning(f"{code_block.origin}: Quarto id does not evaluate to string; skipping")
+            log.warning(f"tried to parse:\n{header}")
+            return
         props.append(Id(attrs["id"]))
 
     if "classes" in attrs.keys():
-        props.extend(Class(c) for c in attrs["classes"])
+        classes = attrs["classes"]
+        if not all(isinstance(c, str) for c in classes):
+            log.warning(f"{code_block.origin}: Quarto classes do not evaluate to strings; skipping")
+            log.warning(f"tried to parse:\n{header}")
+            return
+        props.extend(Class(c) for c in classes)
 
-    props.extend(Attribute(k, v) for k, v in attrs.items()
+    props.extend(Attribute(str(k), str(v)) for k, v in attrs.items()
                  if k not in ("id", "classes"))
 
-    return props
+    code_block.source = body
+    code_block.open_line += "\n" + header
+    code_block.properties.extend(props)
 
 
 @final
@@ -51,11 +83,4 @@ class Hook(HookBase):
     @override
     def on_read(self, code: CodeBlock):
         log.debug("quarto filter: %s", code)
-
-        if code.language is None:
-            return
-
-        extra_props = get_code_properties(code.source, code.language)
-        log.debug("quarto attributes: %s", extra_props)
-
-        code.properties.extend(extra_props)
+        amend_code_properties(code)
--- test/test_quarto_attributes.py
+++ test/test_quarto_attributes.py
@@ -1,13 +1,17 @@
+from pathlib import PurePath
+from entangled.code_reader import CodeReader
+from entangled.commands.stitch import stitch_markdown
 from entangled.config import AnnotationMethod, Markers, config
 from entangled.hooks import get_hooks
 from entangled.markdown_reader import read_markdown_string
 from entangled.tangle import tangle_ref
-from entangled.properties import Attribute
+from entangled.properties import Attribute, get_attribute
 
 
 md_in = """
 ``` {.scheme}
-;| id: hello
+; | id: hello
+; | foo: bar
 (display "hello") (newline)
 ```
 
@@ -20,8 +24,6 @@ md_in = """
 
 
 scm_out = """
-;| file: hello.scm
-;| id: hello
 (display "hello") (newline)
 (display "goodbye") (newline)
 """
@@ -39,8 +41,19 @@ def test_quarto_attributes():
 
     print(hooks)
     refs, _ = read_markdown_string(md_in, hooks=hooks)
+    assert get_attribute(next(iter(refs.by_name("hello"))).properties, "foo") == "bar"
     result, _ = tangle_ref(refs, "hello.scm", AnnotationMethod.NAKED)
     assert result.strip() == scm_out.strip()
+
+
+def test_quarto_attributes_stitch():
+    with config(hooks=["quarto_attributes"]):
+        hooks = get_hooks()
+
+    refs, content = read_markdown_string(md_in, hooks=hooks)
+    source, _ = tangle_ref(refs, "hello.scm")
+    _ = CodeReader(PurePath("-"), refs).run(source)
+    assert stitch_markdown(refs, content) == md_in
 
 
 malformed_attrs = """
