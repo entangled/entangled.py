2c1a1d849e2455900d5adddf36c395beb30b48d5
Johan Hidding <j.hidding@esciencecenter.nl>
2025-11-14 16:38:58 +0100

    port the new command to click

--- entangled/commands/__init__.py
+++ entangled/commands/__init__.py
@@ -1,7 +1,8 @@
 from .main import main
 from .stitch import stitch
 from .tangle import tangle
+from .new import new
 
 __all__ = [
-    "main", "stitch", "tangle"
+    "main", "stitch", "tangle", "new"
 ]
